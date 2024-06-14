aa38467c7130b686e069f924cb2c8aafb4b8518b
mayankraj <rajmayank912@gmail.com>
2024-06-14 16:42:50 +0530

    Completed basic sequelize server for create a db

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
-require('dotenv').config()
+const {PORT} =require('./config/serverConfig')
 
 const setupAndStartserver = async () => {
 
     const app = express();
-    const PORT = 3000;
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-        console.log(process.env);
+        
 
     })
 }
